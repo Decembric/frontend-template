e7df0f62d21cd172f78199b42df52ba26fc91e23
JeinKh <juliakhezhun@gmail.com>
2024-12-04 12:30:20 +0200

    Update operations.js

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -3,24 +3,24 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://back-inter-mafia.onrender.com/api/water";
 
-// export const fetchWaterData = createAsyncThunk(
-//   "water/fetchWaterData",
-//   async ({ type, date }, thunkAPI) => {
-//     try {
-//       const endpoint = type === "month" ? "/month" : "/day";
-//       // console.log(endpoint);
+export const fetchWaterData = createAsyncThunk(
+  "water/fetchWaterData",
+  async ({ type, date }, thunkAPI) => {
+    try {
+      const endpoint = type === "month" ? "/month" : "/day";
+      // console.log(endpoint);
 
-//       const response = await axios.get(endpoint, {
-//         params: { date },
-//       });
+      const response = await axios.get(endpoint, {
+        params: { date },
+      });
 
-//       // console.dir(response.data);
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.response.data);
-//     }
-//   }
-// );
+      // console.dir(response.data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 export const apiDeleteWater = createAsyncThunk(
   "water/apiDeleteWater",
